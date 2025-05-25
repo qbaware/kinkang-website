dfd1390817a0cf48a0884667c5d3c15d003ef77a
Daniel Gospodinow <danielgospodinow@gmail.com>
2025-05-25 18:58:45 +0100

    Wrap up landing page

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,10 +1,11 @@
+import { get } from 'node:http';
 import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
 
 export const headerData = {
   links: [
     {
       text: 'Home',
-      href: getPermalink('/homes/kinkang')
+      href: getPermalink('/homes/kinkang'),
     },
     {
       text: 'Solutions',
@@ -24,13 +25,13 @@ export const headerData = {
       links: [
         {
           text: 'About us',
-          href: getBlogPermalink(),
+          href: getPermalink('/about'),
         },
       ],
     },
     {
       text: 'Pricing',
-      href: getPermalink('/pricing')
+      href: getPermalink('/pricing'),
     },
     {
       text: 'Resources',
@@ -39,12 +40,16 @@ export const headerData = {
           text: 'Blog',
           href: getBlogPermalink(),
         },
+        {
+          text: 'FAQs',
+          href: getPermalink('/faqs'),
+        },
       ],
     },
     {
       text: 'Contact',
       href: getPermalink('/contact'),
-    }
+    },
   ],
 };
 
@@ -107,7 +112,6 @@ export const footerData = {
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
   ],
   footNote: `
-    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
-    Made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
+    Kinkang · All rights reserved.
   `,
 };
